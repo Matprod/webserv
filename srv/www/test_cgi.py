c5992f54105359da309d496ff6b5f196b0dd3be2
Matprod <matprod42@gmail.com>
2025-10-07 15:28:53 +0200

    push

--- srv/www/test_cgi.py
+++ srv/www/test_cgi.py
@@ -7,7 +7,7 @@ print("<html><body>")
 print("<h2>Python CGI Test</h2>")
 print("<p>Method: {}</p>".format(os.environ.get("REQUEST_METHOD", "")))
 if os.environ.get("REQUEST_METHOD") == "POST":
-    form = cgi.FieldStorage()
-    for key in form.keys():
+    form = cgi.FieldStorage()ewrre
+    for key in form.keys():werwer
         print("<p>{}: {}</p>".format(key, form[key].value))
-print("</body></html>")
+print("</body></html>")we rwer werwe rwer 
